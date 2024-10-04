c84cfd10432639b229501a15c67dd80028c7f218
melinamelo <melinamelo0688@gmail.com>
2024-10-04 11:32:45 -0300

    tabelas

---
+++ Models/MaisComentados.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oráculo.Models
+{
+    [Table("MaisComentados")]
+    public class MaisComentados
+    {
+        [Column("MaisComentadosId")]
+        [Display(Name = "Código de Mais comentados")]
+        public int MaisComentadosId { get; set; }
+
+        [ForeignKey("PostagemId")]
+        [Display(Name = "Postagem")]
+        public int PostagemId { get; set; }
+        public Postagem? Postagem { get; set; }
+    }
+}
