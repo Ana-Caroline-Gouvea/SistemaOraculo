7afbae607642a367932e684dcc92f7e6e9b8d162
Ana-Caroline-Gouvea <anagouvea.senai@gmail.com>
2024-10-04 11:44:48 -0300

    Model Usuário

---
+++ Models/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Oráculo.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Column("UsuarioId")]
+        [Display(Name = "Código do Usuário")]
+        public int UsuarioId { get; set; }
+
+        [Column("UsuarioNome")]
+        [Display(Name = "Nome do Usuário")]
+        public string UsuarioNome { get; set; } = string.Empty;
+
+        [Column("UsuarioEmail")]
+        [Display(Name = "Email do Usuário")]
+        public string UsuarioEmail { get; set; } = string.Empty;
+
+        [Column("UsuarioApelido")]
+        [Display(Name = "Apelido do Usuário")]
+        public string UsuarioApelido { get; set; } = string.Empty;
+
+        [Column("UsuarioDataNascimento")]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimeto { get; set; }
+
+        [Column("UsuarioSenha")]
+        [Display(Name = "Senha")]
+        public string UsuarioSenha { get; set; } = string.Empty;
+
+        [Column("UsuarioConfirmarSenha")]
+        [Display(Name = "Confirmar senha")]
+        public string UsuarioConfirmarSenha { get; set; } = string.Empty;
+    }
+}
