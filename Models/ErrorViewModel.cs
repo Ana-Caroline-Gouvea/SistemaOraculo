bb0d0d043114708869d1d50663643c8924f84a85
Ana-Caroline-Gouvea <anagouvea.senai@gmail.com>
2024-10-04 10:15:54 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oráculo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
