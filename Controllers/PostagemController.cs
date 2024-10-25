dae176b8232b011dce65942df3362994b23812ae
melinamelo <melinamelo0688@gmail.com>
2024-10-25 13:54:41 -0300

    aa

--- Controllers/PostagemController.cs
+++ Controllers/PostagemController.cs
@@ -58,6 +58,7 @@ namespace Oraculo.Controllers
             return View();
         }
 
+
         // POST: Postagem/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
